7f9ed8d5f23041ef41121d32193c6bf3257e256b
Angelo94a <angelo94.aa@gmail.com>
2024-11-04 22:12:07 -0500

    🚀

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Header from './Header.astro'
+import Footer from './Footer.astro'
+import LiveDay from './LiveDay.astro'
+import Seo from './Seo.astro'
+
+export { Header, Footer, LiveDay, Seo }
---
+++ src/section/index.ts
@@ -0,0 +1,7 @@
+import Unete from './Unete.astro'
+import EsteEvento from './EsteEvento.astro'
+import LideresEnCx from './LideresEnCx.astro'
+import Testimonios from './Testimonios.astro'
+import Form from './Form.astro'
+
+export { Unete, EsteEvento, LideresEnCx, Testimonios, Form }
