27c27693a852a248a997163f9903381fc2938836
Angelo94a <angelo94.aa@gmail.com>
2024-11-05 10:30:54 -0500

    Mejorando los estilos

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,7 @@ import Header from './Header.astro'
 import Footer from './Footer.astro'
 import LiveDay from './LiveDay.astro'
 import Seo from './Seo.astro'
+import Container from './Container.astro'
+import CardLideres from './CardLideres.astro'
 
-export { Header, Footer, LiveDay, Seo }
+export { Header, Footer, LiveDay, Seo, Container, CardLideres }
